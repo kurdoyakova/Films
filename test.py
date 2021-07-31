f9b9d5238942b93a214aff87f63712eca2e35f09
kurdoyakova <kurdoyakovams@yandex.ru>
2021-07-31 21:30:52 +0300

    norm print info

--- test.py
+++ test.py
@@ -2,204 +2,238 @@ import requests
 from bs4 import BeautifulSoup
 from tabulate import tabulate
 
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.150 Safari/537.36",
+headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) \
+Chrome/88.0.4324.150 Safari/537.36",
 "Accept-language":"ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
-"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"}
+"Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,\
+*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"}
 
-def print_info_film(type_genre,ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,rating_imdb,actor,storyline):
-	table = [['type', type_genre],['ru_name', ru_name], ['eng_name', eng_name], ['release_year', release_year],
-		['country', country], ['producer', producer], ['runtime', runtime],
-		['genres', genre], ['rating_kp', rating_kp], ['rating_imdb', rating_imdb],['actors', actor], ['storyline', storyline]]
-	print(tabulate(table, tablefmt="plain"))
 
-def print_info_serail(type_genre,number_of_seasons,ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,rating_imdb,actor,storyline):
-	table = [['type', type_genre],['number of seasons', number_of_seasons],['ru_name', ru_name], ['eng_name', eng_name], ['release_year', release_year],
-		['country', country], ['producer', producer], ['runtime', runtime],
-		['genres', genre], ['rating_kp', rating_kp], ['rating_imdb', rating_imdb],['actors', actor], ['storyline', storyline]]
-	print(tabulate(table, tablefmt="plain"))
+class Film():
+    """Class for describing film info"""
+    def __init__(self, genres=None):
+        if genres:
+            self.genres = genres
+        else:
+            self.genres = []
 
-def kp_type(url):	
-	response = requests.get(url, headers = headers)
-	if response.status_code == 200:
-		print("-- kinopoisk works --")
-	elif response.status_code == 404:
-		print('-- not found --')
-	else:
-		print("-- something is wrong --",response.status_code)
-	soup = BeautifulSoup(response.content, "html.parser")
+    def __repr__(self):
+        repr_str = 'Genres: ' + ', '.join(self.genres)
+        return repr_str
 
-	quotes_for_type = soup.find_all('div', class_='styles_title__1yvs9')
-	if quotes_for_type == []:
-		quotes_for_type = soup.find_all('div', class_='styles_title__3tVSa')
-		type_with_year = quotes_for_type[0].contents[0].contents[2].text
-		type_genre = type_with_year[1:7]
-	else:
-		type_genre = 'фильм'
-	return type_genre,soup
 
-def kp_info_film(url,soup):
-	quotes = soup.find_all('div', class_="styles_rowDark__2qC4I styles_row__2ee6F")
-	quotes2 = soup.find_all('div', class_='styles_title__1yvs9')
-	quotes3 = soup.find_all('div', class_='styles_body__2Ynqg')
-	quotes4 = soup.find_all('div', class_='styles_filmSynopsis__zLClu')
-	quotes5 = soup.find_all('div', class_="styles_actors__2zt1j")
+def print_info(**kwargs):
+    """printing information in the table"""
+    table = []
+    for k, v in kwargs.items():
+        table.append([str(k), v])
+    print(tabulate(table, tablefmt="plain"))
 
-	release_year = (quotes[0].contents)[1].text
-	ru_name_with_year = (quotes2[0].contents)[0].contents[0].text
-	ru_name = ru_name_with_year[:len(ru_name_with_year)-6]
-	eng_name = (quotes2[0].contents)[1].contents[0].text
-	country = (quotes[1].contents)[1].text
-	rating_kp = (quotes3[0].contents)[0].text
 
-	runtime_full = (quotes[len(quotes)-1].contents)[1].text
-	for i in runtime_full:
-		if i == '/':
-			runtime = runtime_full[len(runtime_full)-6:len(runtime_full)]
-			break
-		else:
-			runtime = runtime_full[0:len(runtime_full)-1]
+def resnonse_url(url):
+    """receiving a response from the server"""
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        print("-- works --")
+    elif response.status_code == 404:
+        print('-- not found --')
+    else:
+        print("-- something is wrong --", response.status_code)
+    soup = BeautifulSoup(response.content, "html.parser")
+    return soup
 
-	genre = []
-	genres = (quotes[2].contents)[1].contents[0].contents
-	for i in range(0,len(genres),2):
-		genre.append(genres[i].text)
 
-	producer = []
-	producers = (quotes[4].contents)[1].contents
-	for i in range(0,len(producers),2):
-		if producers[i].text == '...':
-			break
-		producer.append(producers[i].text)
+def kp_type(url):
+    """type definition"""
+    soup = resnonse_url(url)
+    quotes_for_type = soup.find_all('div', class_='styles_title__1yvs9')
+    if quotes_for_type == []:
+        quotes_for_type = soup.find_all('div', class_='styles_title__3tVSa')
+        type_with_year = quotes_for_type[0].contents[0].contents[2].text
+        type_genre = type_with_year[1:7]
+    else:
+        type_genre = 'фильм'
+    return type_genre, soup
 
-	if quotes4 == []:
-		storyline = '-'
-	else:
-		storyline = quotes4[0].text
 
-	actor = []
-	actors = (quotes5[0].contents)[1].contents
-	for i in range(0,len(actors)):
-		actor.append(actors[i].text)
+def kp_info_film(soup):
+    """getting basic information about a movie on the kinopoisk"""
+    quotes = soup.find_all(
+        'div', class_="styles_rowDark__2qC4I styles_row__2ee6F")
+    if quotes == []:
+        quotes = soup.find_all(
+            'div', class_='styles_rowLight__3uy9z styles_row__2ee6F')
+    quotes2 = soup.find_all('div', class_='styles_title__1yvs9')
+    quotes3 = soup.find_all('div', class_='styles_body__2Ynqg')
+    quotes4 = soup.find_all('div', class_='styles_filmSynopsis__zLClu')
+    quotes5 = soup.find_all('div', class_="styles_actors__2zt1j")
 
-	return ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,actor,storyline
+    release_year = (quotes[0].contents)[1].text
+    ru_name_with_year = (quotes2[0].contents)[0].contents[0].text
+    ru_name = ru_name_with_year[:len(ru_name_with_year)-6]
+    eng_name = (quotes2[0].contents)[1].contents[0].text
+    rating_kp = (quotes3[0].contents)[0].text
 
-def kp_info_serial(url,soup):
-	quotes = soup.find_all('div', class_='styles_rowLight__3uy9z styles_row__2ee6F')
-	quotes2 = soup.find_all('div', class_='styles_title__3tVSa')
-	quotes3 = soup.find_all('div', class_='styles_body__2Ynqg')
-	quotes4 = soup.find_all('div', class_='styles_filmSynopsis__zLClu')
-	quotes5 = soup.find_all('div', class_="styles_actors__2zt1j")
+    runtime_full = (quotes[len(quotes)-1].contents)[1].text
+    for i in runtime_full:
+        if i == '/':
+            runtime = runtime_full[len(runtime_full)-6:len(runtime_full)]
+            break
+        runtime = runtime_full[0:len(runtime_full)-1]
 
-	release_year_with_number_of_seasons = (quotes[0].contents)[1].text
-	release_year = release_year_with_number_of_seasons[0:4]
-	number_of_seasons = release_year_with_number_of_seasons[6:7]
-	ru_name = (quotes2[0].contents)[0].contents[0].text
-	eng_name = (quotes2[0].contents)[1].contents[0].text
-	country = (quotes[1].contents)[1].text
-	rating_kp = (quotes3[0].contents)[0].text
+    genre = []
+    genres = (quotes[2].contents)[1].contents[0].contents
+    for i in range(0, len(genres), 2):
+        genre.append(genres[i].text)
 
-	runtime_full = (quotes[len(quotes)-1].contents)[1].text
-	for i in runtime_full:
-		if i == '/':
-			runtime = runtime_full[len(runtime_full)-6:len(runtime_full)]
-			break
-		else:
-			runtime = runtime_full[0:len(runtime_full)-1]
+    country = []
+    countrys = quotes[1].contents[1].contents
+    for i in range(0, len(countrys), 2):
+        country.append(countrys[i].text)
 
-	genre = []
-	genres = (quotes[2].contents)[1].contents[0].contents
-	for i in range(0,len(genres),2):
-		genre.append(genres[i].text)
+    producer = []
+    producers = (quotes[4].contents)[1].contents
+    for i in range(0, len(producers), 2):
+        if producers[i].text == '...':
+            break
+        producer.append(producers[i].text)
 
-	producer = []
-	producers = (quotes[4].contents)[1].contents
-	for i in range(0,len(producers),2):
-		if producers[i].text == '...':
-			break
-		producer.append(producers[i].text)
+    if quotes4 == []:
+        storyline = '-'
+    else:
+        storyline = quotes4[0].text
 
-	if quotes4 == []:
-		storyline = '-'
-	else:
-		storyline = quotes4[0].text
+    actor = []
+    actors = (quotes5[0].contents)[1].contents
+    for i, _ in enumerate(actors):
+        actor.append(actors[i].text)
 
-	actor = []
-	actors = (quotes5[0].contents)[1].contents
-	for i in range(0,len(actors)):
-		actor.append(actors[i].text)
+    return ru_name, eng_name, release_year, country,\
+        producer, runtime, genre, rating_kp, actor, storyline
 
-	return number_of_seasons,ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,actor,storyline
+
+def kp_info_serial(soup):
+    """getting basic information about the series on the kinopoisk"""
+    quotes = soup.find_all(
+        'div', class_="styles_rowDark__2qC4I styles_row__2ee6F")
+    if quotes == []:
+        quotes = soup.find_all(
+            'div', class_='styles_rowLight__3uy9z styles_row__2ee6F')
+    quotes2 = soup.find_all('div', class_='styles_title__3tVSa')
+    quotes3 = soup.find_all('div', class_='styles_body__2Ynqg')
+    quotes4 = soup.find_all('div', class_='styles_filmSynopsis__zLClu')
+    quotes5 = soup.find_all('div', class_="styles_actors__2zt1j")
+
+    release_year_with_number_of_seasons = (quotes[0].contents)[1].text
+    release_year = release_year_with_number_of_seasons[0:4]
+    number_of_seasons = release_year_with_number_of_seasons[6:7]
+    ru_name = (quotes2[0].contents)[0].contents[0].text
+    eng_name = (quotes2[0].contents)[1].contents[0].text
+    country = (quotes[1].contents)[1].text
+    rating_kp = (quotes3[0].contents)[0].text
+
+    runtime_full = (quotes[len(quotes)-1].contents)[1].text
+    for i in runtime_full:
+        if i == '/':
+            runtime = runtime_full[len(runtime_full)-6:len(runtime_full)]
+            break
+        runtime = runtime_full[0:len(runtime_full)-1]
+
+    genre = []
+    genres = (quotes[2].contents)[1].contents[0].contents
+    for i in range(0, len(genres), 2):
+        genre.append(genres[i].text)
+
+    producer = []
+    producers = (quotes[4].contents)[1].contents
+    for i in range(0, len(producers), 2):
+        if producers[i].text == '...':
+            break
+        producer.append(producers[i].text)
+
+    if quotes4 == []:
+        storyline = '-'
+    else:
+        storyline = quotes4[0].text
+
+    actor = []
+    actors = (quotes5[0].contents)[1].contents
+    for i, _ in enumerate(actors):
+        actor.append(actors[i].text)
+
+    return number_of_seasons, ru_name, eng_name, release_year, country,\
+        producer, runtime, genre, rating_kp, actor, storyline
+
 
 def imdb_info(url):
-	response = requests.get(url, headers = headers)
-	if response.status_code == 200:
-		print("-- IMDb works --")
-	else:
-		print("-- something is wrong --",response.status_code)
-	soup = BeautifulSoup(response.content, "html.parser")
+    """getting basic information on the IMDb website"""
+    soup = resnonse_url(url)
+    quotes_for_rating_imdb = soup.find_all(
+        'div', class_="AggregateRatingButton__ContentWrap-sc-1ll29m0-0 hmJkIS")
+    rating_imdb = (quotes_for_rating_imdb[0].contents)[0].contents[0].text
+    return rating_imdb
 
-	quotes_for_rating_imdb = soup.find_all('div', class_="AggregateRatingButton__ContentWrap-sc-1ll29m0-0 hmJkIS")
-	rating_imdb = (quotes_for_rating_imdb[0].contents)[0].contents[0].text
-	return rating_imdb
 
 def find_url_kp(name):
-	words = name.split()
-	name_like_url = words[0]
-	for i in range(1,len(words)):
-		name_like_url = str(name_like_url) + '+' + str(words[i])
-	url = 'https://www.kinopoisk.ru/index.php?kp_query=' + str(name_like_url)
+    """getting url on the kinopoisk"""
+    words = name.split()
+    name_like_url = words[0]
+    for i in range(1, len(words)):
+        name_like_url = str(name_like_url) + '+' + str(words[i])
+    url = 'https://www.kinopoisk.ru/index.php?kp_query=' + str(name_like_url)
+    soup = resnonse_url(url)
+    quotes_for_id_kp = soup.find_all('p', class_='name')
+    id_kp = quotes_for_id_kp[0].contents[0].get('data-url')
+    url_kp = 'https://www.kinopoisk.ru' + str(id_kp)
+    return url_kp
 
-	response = requests.get(url, headers = headers)
-	if response.status_code == 200:
-		print("-- kinopoisk works --")
-	elif response.status_code == 404:
-		print('-- not found --')
-	else:
-		print("-- something is wrong --",response.status_code)
-	soup = BeautifulSoup(response.content, "html.parser")
-
-	quotes_for_id_kp = soup.find_all('p', class_='name')
-	id_kp = quotes_for_id_kp[0].contents[0].get('data-url')
-	url_kp =  'https://www.kinopoisk.ru' + str(id_kp)
-	return url_kp
 
 def find_url_imdb(name):
-	words = name.split()
-	name_like_url = words[0]
-	for i in range(1,len(words)):
-		name_like_url = str(name_like_url) + '+' + str(words[i])
-	url = 'https://www.imdb.com/find?q=' + str(name_like_url) + '&ref_=nv_sr_sm'
+    """getting url on the IMDb"""
+    words = name.split()
+    name_like_url = words[0]
+    for i in range(1, len(words)):
+        name_like_url = str(name_like_url) + '+' + str(words[i])
+    url = 'https://www.imdb.com/find?q=' + \
+        str(name_like_url) + '&ref_=nv_sr_sm'
+    soup = resnonse_url(url)
+    quotes_for_id_imdb = soup.find_all('table', class_='findList')
+    quotes_cool = quotes_for_id_imdb[0].contents[1].contents[1].contents[1]
+    id_imdb = quotes_cool.get('href')
+    url_imdb = 'https://www.imdb.com' + str(id_imdb)
+    return url_imdb
 
-	response = requests.get(url, headers = headers)
-	if response.status_code == 200:
-		print("-- IMDb works --")
-	elif response.status_code == 404:
-		print('-- not found --')
-	else:
-		print("-- something is wrong --",response.status_code)
-	soup = BeautifulSoup(response.content, "html.parser")
-
-	quotes_for_id_imdb = soup.find_all('table', class_='findList')
-	quotes_cool = quotes_for_id_imdb[0].contents[1].contents[1].contents[1]
-	id_imdb = quotes_cool.get('href')
-	url_imdb =  'https://www.imdb.com' + str(id_imdb)
-	return url_imdb
 
 def main(name):
-	url_kp = find_url_kp(name)
-	type_genre,soup = kp_type(url_kp)
+    """the main task"""
+    url_kp = find_url_kp(name)
+    type_genre, soup = kp_type(url_kp)
+    if type_genre == 'фильм':
+        # film = Film()
+        ru_name, eng_name, release_year, country, producer, runtime, genre,\
+            rating_kp, actor, storyline = kp_info_film(soup)
 
-	if type_genre == 'фильм':
-		ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,actor,storyline = kp_info_film(url_kp,soup)
-		url_imdb = find_url_imdb(eng_name)
-		rating_imdb = imdb_info(url_imdb)
-		print_info_film(type_genre,ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,rating_imdb,actor,storyline)
-	else:
-		number_of_seasons,ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,actor,storyline = kp_info_serial(url_kp,soup)
-		url_imdb = find_url_imdb(eng_name)
-		rating_imdb = imdb_info(url_imdb)
-		print_info_serail(type_genre,number_of_seasons,ru_name,eng_name,release_year,country,producer,runtime,genre,rating_kp,rating_imdb,actor,storyline)
+        url_imdb = find_url_imdb(eng_name)
+        rating_imdb = imdb_info(url_imdb)
+        print_info(type=type_genre, ru_name=ru_name,
+                   eng_name=eng_name, release_year=release_year,
+                   country=country, producer=producer, runtime=runtime,
+                   genre=genre, rating_kp=rating_kp,
+                   rating_imdb=rating_imdb, actor=actor,
+                   storyline=storyline)
+    else:
+        number_of_seasons, ru_name, eng_name, release_year, country, producer,\
+            runtime, genre, rating_kp, actor, storyline = kp_info_serial(soup)
+        url_imdb = find_url_imdb(eng_name)
+        rating_imdb = imdb_info(url_imdb)
+        print_info(type=type_genre, number_of_seasons=number_of_seasons,
+                   ru_name=ru_name, eng_name=eng_name,
+                   release_year=release_year, country=country,
+                   producer=producer, runtime=runtime, genre=genre,
+                   rating_kp=rating_kp, rating_imdb=rating_imdb,
+                   actor=actor, storyline=storyline)
+
 
 if __name__ == '__main__':
-	name = 'грань'
-	main(name)
+    NAME = 'рик и морти'
+    main(NAME)
